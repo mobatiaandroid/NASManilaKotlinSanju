f6661e12ec1d33405643018c6213be75f3c4c38c
mobatiaandroid <sanju.sabu@mobatia.com>
2021-06-10 12:00:18 +0530

    Initial commit

---
+++ app/src/main/java/com/mobatia/nasmanila/constants/JSONConstants.kt
@@ -0,0 +1,4 @@
+package com.mobatia.nasmanila.constants
+
+class JSONConstants {
+}
---
+++ app/src/main/java/com/mobatia/nasmanila/constants/URLConstants.kt
@@ -0,0 +1,9 @@
+package com.mobatia.nasmanila.constants
+
+class URLConstants {
+    companion object{
+
+        var URL_HEAD = " http://manila.mobatia.in:8081/"
+
+    }
+}
